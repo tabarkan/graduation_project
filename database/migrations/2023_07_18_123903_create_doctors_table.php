deccb214dcb67b82a57f81606f4f5b2fffe789b7
tabarkan <tabarkan63@gmail.com>
2023-07-18 16:14:04 +0300

    migrations done

--- database/migrations/2023_07_18_123903_create_doctors_table.php
+++ database/migrations/2023_07_18_123903_create_doctors_table.php
@@ -11,17 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('doctors', function (Blueprint $table) {
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('email')->unique();        
             $table->string('phone');
-            $table->integer('role');
-            $table->string('gender');
-            $table->date('birth_date');
+            $table->string('specialization');
+            $table->string('address');
+            $table->string('hospital');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -32,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('doctors');
     }
 };
---
+++ database/migrations/2023_07_18_124855_create_diseases_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('diseases', function (Blueprint $table) {
+            $table->id();
+            $table->string('disease_name');
+            $table->string('disease_category')->unique();        
+            $table->longText('treatment');
+            $table->longText('protection');
+            $table->longText('symptoms');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('diseases');
+    }
+};
