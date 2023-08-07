c9874bbc94843c1c0c351e8a0855c838a11ca87a
tabarkan <tabarkan63@gmail.com>
2023-08-07 14:24:50 +0300

    button

--- resources/views/user/add-hospital.blade.php
+++ resources/views/user/add-hospital.blade.php
@@ -77,6 +77,7 @@
                       </div>
                       
                         <div class="text-center">
+                          <button type="submit" class="btn-close" aria-label="Close">ADD</button>
                           <button type="submit"class="inline-block w-full px-5 py-2.5 mt-6 mb-2 font-bold text-center text-white align-middle transition-all bg-transparent border-0 rounded-lg cursor-pointer active:opacity-85 hover:-translate-y-px hover:shadow-xs leading-normal text-sm ease-in tracking-tight-rem shadow-md bg-150 bg-x-25 bg-gradient-to-tl from-zinc-800 to-zinc-700 hover:border-slate-700 hover:bg-slate-700 hover:text-white">Add</button>
                         </div>
                       </form>
