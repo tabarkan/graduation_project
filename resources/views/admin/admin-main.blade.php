ebeee5e9ff97da3c248d82d14eacb782a2e52273
tabarkan <tabarkan63@gmail.com>
2023-08-18 16:43:22 +0300

    edit some erorr

--- resources/views/admin/admin-main.blade.php
+++ resources/views/admin/admin-main.blade.php
@@ -28,12 +28,12 @@
   
             <div class="flex items-center mt-2 grow sm:mt-0 sm:mr-6 md:mr-0 lg:flex lg:basis-auto">
               <div class="flex items-center md:ml-auto md:pr-4">
-                <div class="relative flex flex-wrap items-stretch w-full transition-all rounded-lg ease">
+                {{-- <div class="relative flex flex-wrap items-stretch w-full transition-all rounded-lg ease">
                   <span class="text-sm ease leading-5.6 absolute z-50 -ml-px flex h-full items-center whitespace-nowrap rounded-lg rounded-tr-none rounded-br-none border border-r-0 border-transparent bg-transparent py-2 px-2.5 text-center font-normal text-slate-500 transition-all">
                     <i class="fas fa-search"></i>
                   </span>
                   <input type="text" class="pl-9 text-sm focus:shadow-primary-outline ease w-1/100 leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300   bg-white bg-clip-padding py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-blue-500 focus:outline-none focus:transition-shadow" placeholder="Type here..." />
-                </div>
+                </div> --}}
               </div>
               <ul class="flex flex-row justify-end pl-0 mb-0 list-none md-max:w-full">
                 <!-- online builder btn  -->
@@ -42,7 +42,8 @@
                 </li> -->
                 <li class="flex items-center">
                   <a href="../pages/sign-in.html" class="block px-0 py-2 text-sm font-semibold text-white transition-all ease-nav-brand">
-                    <i class="fa fa-user sm:mr-1"></i>
+                    {{-- <i class="fa fa-user sm:mr-1"></i> --}}
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M5 21q-.825 0-1.413-.588T3 19V5q0-.825.588-1.413T5 3h7v2H5v14h7v2H5Zm11-4l-1.375-1.45l2.55-2.55H9v-2h8.175l-2.55-2.55L16 7l5 5l-5 5Z"/></svg>
                     <span class="hidden sm:inline"></span>
                   </a>
                 </li>
@@ -56,8 +57,8 @@
                   </a>
                 </li>
                 <li class="flex items-center px-4">
-                  <a href="javascript:;" class="p-0 text-sm text-white transition-all ease-nav-brand">
-                    <i fixed-plugin-button-nav class="cursor-pointer fa fa-cog"></i>
+                  <a href=""  class="p-0 text-sm text-white transition-all ease-nav-brand">
+                    <a href="{{route('admin.main')}}" class="p-0 text-sm text-white transition-all ease-nav-brand"><i fixed-plugin-button-nav class="cursor-pointer fa fa-cog"></i> </a>
                     <!-- fixed-plugin-button-nav  -->
                   </a>
                 </li>
@@ -67,7 +68,7 @@
                 <li class="relative flex items-center pr-2">
                   <p class="hidden transform-dropdown-show"></p>
                   <a href="javascript:;" class="block p-0 text-sm text-white transition-all ease-nav-brand" dropdown-trigger aria-expanded="false">
-                    <i class="cursor-pointer fa fa-bell"></i>
+                   <a href="{{route('notifications.list')}}" class="block p-0 text-sm text-white transition-all ease-nav-brand"> <i class="cursor-pointer fa fa-bell"></i> </a>
                   </a>
   
                   <ul dropdown-menu class="text-sm transform-dropdown before:font-awesome before:leading-default before:duration-350 before:ease lg:shadow-3xl duration-250 min-w-44 before:sm:right-8 before:text-5.5 pointer-events-none absolute right-0 top-0 z-50 origin-top list-none rounded-lg border-0 border-solid border-transparent bg-white bg-clip-padding px-2 py-4 text-left text-slate-500 opacity-0 transition-all before:absolute before:right-2 before:left-auto before:top-0 before:z-50 before:inline-block before:font-normal before:text-white before:antialiased before:transition-all before:content-['\f0d8'] sm:-mr-6 lg:absolute lg:right-0 lg:left-auto lg:mt-2 lg:block lg:cursor-pointer">
